dfcc26ea648851f5df9539acebb69334d43e64c1
Evan Mosseri <evan.mosseri@gmail.com>
2015-07-18 10:52:09 -0400

    deleted unneccessary files

--- bench.py
+++
@@ -1,9 +0,0 @@
-def test():
-    """Stupid test function"""
-    L = []
-    for i in range(100):
-        L.append(i)
-
-if __name__ == '__main__':
-    import timeit
-    print(timeit.timeit("test()", setup="from __main__ import test"))
